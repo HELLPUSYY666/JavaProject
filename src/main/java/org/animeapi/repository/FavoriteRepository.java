c32c95060917d9f1c2134d547f159de9ddd4322a
Zakariya <pzakariya@mail.ru>
2025-04-04 20:47:55 +0500

    full logic for favorite

--- src/main/java/org/animeapi/repository/FavoriteRepository.java
+++ src/main/java/org/animeapi/repository/FavoriteRepository.java
@@ -1,20 +1,16 @@
 package org.animeapi.repository;
 
-// Favorite Repository
-
 import org.animeapi.model.Favorite;
 import org.animeapi.model.FavoriteId;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, FavoriteId> {
-    @Query("SELECT f FROM Favorite f")
-    List<Favorite> findAllFavorites();
 
-    @Query("SELECT f FROM Favorite f WHERE f.user.userId = :userId AND f.anime.animeId = :animeId")
-    Optional<Favorite> findFavoriteById(@Param("userId") Integer userId, @Param("animeId") Integer animeId);
+    List<Favorite> findByUser_UserId(Integer userId);
+
+    void deleteByUser_UserIdAndAnime_AnimeId(Integer userId, Integer animeId);
+
+    boolean existsByUser_UserIdAndAnime_AnimeId(Integer userId, Integer animeId);
 }
